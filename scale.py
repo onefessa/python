46e0841cf6f3142d62c143ab675072de2331c6d7
onefessa <63928066+onefessa@users.noreply.github.com>
2020-05-11 04:57:02 +0300

    Add files via upload

---
+++ scale.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+from scipy import misc
+from imageio import imread, imwrite
+
+print('Scale:')
+N = input()
+n = int(N)
+print('InputFile:')
+inputfile=input()
+print('OutputFile:')
+outputfile=input()
+im1=imread(inputfile)
+#im1=imread('31.jpg')
+h=len(im1)      # высота изображения = длина списка строк изображения
+w=len(im1[0])   # ширина изображения = длина списка одной строки
+print('width=',w, 'height=',h, 'size=',w*h*4)
+
+im2=np.ones((n*h,n*w,3))
+im1=im1.astype('uint8')
+im2=im2.astype('uint8')
+
+for ix in range (0, w):
+    print(ix);
+    for iy in range(0, h):
+        for i in range ((ix)*n,(ix+1)*n):
+            for j in range ((iy)*n,(iy+1)*n):
+                im2[j][i]=im1[iy][ix]
+                #im2[j][i][1]=im1[iy][ix][1]
+                #im2[j][i][2]=im1[iy][ix][2]
+imwrite(outputfile,im2,format='bmp')
